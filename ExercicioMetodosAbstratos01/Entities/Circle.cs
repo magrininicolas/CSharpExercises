f53da8c4a8ab048fea9599cf5ad988a2c369efb5
Nicolas <nicolas@pop-os.localdomain>
2023-03-13 23:18:09 -0300

    Adicionadas os exercicios ExercicioMetodosAbstratos01 e ExercicioMetodosAbstratos02

---
+++ ExercicioMetodosAbstratos01/Entities/Circle.cs
@@ -0,0 +1,19 @@
+using Course.Entities.Enums;
+
+namespace Course.Entities
+{
+  class Circle : Shape 
+  {
+    public double Radius { get; set; }
+
+    public Circle(double radius, Color color) : base(color)
+    {
+      Radius = radius;
+    }
+
+    public override double Area()
+    {
+      return 3.14 * Math.Pow(Radius, 2.0);
+    }
+  }
+}
---
+++ ExercicioMetodosAbstratos01/Program.cs
@@ -0,0 +1,45 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<Shape> list = new List<Shape>();
+
+      Console.Write("Enter the number of shapes: ");
+      int n = int.Parse(Console.ReadLine());
+
+      for(int i = 1; i<=n; i++)
+      {
+        Console.Write($"Shape #{i} data");
+        Console.Write("Rectangle or circle (r/c)? ");
+        char resp = char.Parse(Console.ReadLine());
+        Console.Write("Color (black/blue/red): ");
+        Color color = Color.Parse<Color>(Console.ReadLine());
+        if(resp == 'r')
+        {
+          Console.Write("Width: ");
+          double width = double.Parse(Console.ReadLine());
+          Console.Write("Height: ");
+          double height = double.Parse(Console.ReadLine());
+          list.Add(new Rectangle(width, height, color));
+        }
+        else if(resp == 'c') 
+        {
+          Console.Write("Radius: ");
+          double radius = double.Parse(Console.ReadLine());
+          list.Add(new Circle(radius, color));
+        }
+      }
+
+      Console.WriteLine("\nShape Areas: ");
+      foreach(Shape fig in list) 
+      {
+        Console.WriteLine($"Color: {fig.Color}, Area: {fig.Area()}");
+      }
+    }
+  }
+}
